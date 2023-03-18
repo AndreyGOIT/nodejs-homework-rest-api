8a3a8771dddc3de9f4d05a032bddd3b0b2e987dc
AndreyGOIT <ae.borga@gmail.com>
2023-03-18 12:11:04 +0200

    upload

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
+const createError = require("http-errors");
 const express = require("express");
+const path = require("path");
+const fs = require("fs").promises;
 const cors = require("cors");
+const multer = require("multer");
+const uploadDir = path.join(process.cwd(), "uploads");
+const storeImage = path.join(process.cwd(), "images");
 
 require("colors");
 require("dotenv").config();
@@ -11,6 +17,37 @@ const app = express();
 // parse application/json
 app.use(express.json());
 app.use(express.static("public"));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/upload", upload.single("picture"), async (req, res, next) => {
+  const { description } = req.body;
+  const { path: temporaryName, originalname } = req.file;
+  const fileName = path.join(storeImage, originalname);
+  try {
+    await fs.rename(temporaryName, fileName);
+  } catch (error) {
+    await fs.unlink(temporaryName);
+    return next(createError(500));
+  }
+  res.json({
+    description,
+    message: "Image uploaded successfully",
+    status: 200,
+  });
+});
 
 require("./config/config-passport");
 
@@ -53,7 +90,22 @@ app.use((err, req, res, next) => {
   next();
 });
 
+const isAccessible = (path) => {
+  return fs
+    .access(path)
+    .then(() => true)
+    .catch(() => false);
+};
+
+const createFolderIsNotExist = async (folder) => {
+  if (!(await isAccessible(folder))) {
+    await fs.mkdir(folder);
+  }
+};
+
 app.listen(PORT, async () => {
+  createFolderIsNotExist(uploadDir);
+  createFolderIsNotExist(storeImage);
   console.log("db connecting...".bgGray.bold.italic);
   await connectDB();
   console.log(
